322ce885b7e665444c2984429b9cab1c1e171e1e
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-09-11 14:05:14 +0200

    Selected all elements with querySelectors

---
+++ index.js
@@ -0,0 +1,12 @@
+"use stric";
+
+// ELEMENTS SELECTED
+const player0 = document.querySelector(".player1");
+const player1 = document.querySelector(".player2");
+const score0 = document.querySelector("#score--0");
+const score1 = document.querySelector("#score--1");
+const current0 = document.querySelector("#current--score1");
+const current1 = document.querySelector("#current--score2");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
