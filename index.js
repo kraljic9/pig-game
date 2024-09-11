21e2102e0bc342a9ee471377e060ea6e3352dd4c
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-09-11 14:13:43 +0200

    Added hidden style for dice in css, added default screen and added dice roll function

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 // ELEMENTS SELECTED
 const player0 = document.querySelector(".player1");
 const player1 = document.querySelector(".player2");
-const score0 = document.querySelector("#score--0");
-const score1 = document.querySelector("#score--1");
+const score0 = document.querySelector("#score--1");
+const score1 = document.querySelector("#score--2");
 const current0 = document.querySelector("#current--score1");
 const current1 = document.querySelector("#current--score2");
+const dice = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
@@ -16,3 +17,15 @@ const btnHold = document.querySelector(".btn--hold");
 let playerScores = [0, 0];
 let currentPlayer = 0;
 let currentScore = 0;
+
+// DEFAULT SCREEN
+
+score0.textContent = 0;
+score1.textContent = 0;
+current0.textContent = 0;
+current1.textContent = 0;
+dice.classList.add("hidden");
+
+// DICE ROLL
+
+btnRoll.addEventListener("click", function () {});
